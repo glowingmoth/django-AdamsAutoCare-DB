463583513b23ee1e8c821bcc529adbfb961bddab
Thomas Doig <thomasdoig@hotmail.com>
2021-06-21 11:57:04 +1000

    fixed typo in setting.py which couldn't read the .env

--- adamsDb/settings.py
+++ adamsDb/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import environ
 env = environ.Env()
-environ.EV.read_env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
