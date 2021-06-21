aeacc43b1b4fbccccf3e173d8d582f784c60faae
Thomas Doig <thomasdoig@hotmail.com>
2021-06-21 22:12:43 +1000

    styled customer creation form

--- customers/forms.py
+++ customers/forms.py
@@ -1,9 +1,19 @@
 from  django import forms
 from django.forms import ModelForm
-from .models import Customer
+from .models import *
 
 
 class CustomerForm(ModelForm):
     class Meta:
         model = Customer
+        fields = '__all__'
+
+class AddressForm(ModelForm):
+    class Meta:
+        model = Address
+        fields = '__all__'
+
+class CarForm(ModelForm):
+    class Meta:
+        model = Car
         fields = '__all__'
--- customers/views.py
+++ customers/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import CustomerForm
+from .forms import *
 
 
 from .models import *
@@ -20,14 +20,16 @@ def detail(request, id):
 
 def create(request):
 
-    form = CustomerForm
+    customer_form = CustomerForm
+    address_form = AddressForm
+    car_form = CarForm
     
     if request.method == 'POST':
-        form = CustomerForm(request.POST)
-        if form.is_valid():
-            form.save()
+        customer_form = CustomerForm(request.POST)
+        if customer_form.is_valid():
+            customer_form.save()
     
-    return render(request, 'customers/create.html', { "form": form })
+    return render(request, 'customers/create.html', { "customer_form": customer_form, "address_form": address_form, "car_form": car_form })
 
 def delete(request, id):
     remove_customer = Customer.objects.get(id=id).delete()
